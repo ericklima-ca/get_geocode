43201e77de43aac0ccf3bd6fdd7553941681a788
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-23 19:51:32 -0400

    implement tests for new functionalities in get_geocode function in Builder module

--- test/builder_test.exs
+++ test/builder_test.exs
@@ -1,22 +1,22 @@
 defmodule GetGeocodeBuilderTest do
-    use ExUnit.Case, async: true
-    import GetGeocode.Builder
+  use ExUnit.Case, async: true
+  import GetGeocode.Builder
 
-    @input_viacep "69035350"
-    @input_nominatim "Rua Izaurina Braga, Compensa, Amazonas"
+  @input_viacep "69035350"
+  @input_nominatim "Rua Izaurina Braga, Compensa, Amazonas"
 
-    test "get_geocode/1 from viacep" do
-        %{"localidade" => cidade} = get_geocode(@input_viacep)
-        assert cidade == "Manaus"
-    end
+  test "get_geocode/1 from viacep" do
+    result = get_geocode(@input_viacep)
+    assert result =~ "postalcode"
+  end
 
-    test "get_geocode/1 from nominatim" do
-        result = get_geocode(@input_nominatim)
-        assert Map.has_key?(result, "lon")
-    end
+  test "get_geocode/1 from nominatim" do
+    result = get_geocode(@input_nominatim)
+    assert result =~ "lng"
+  end
 
-    test "get_geocode/1 returning error message" do
-        %{msg: msg} = get_geocode("?")
-        assert msg == "Invalid input"
-    end
+  test "get_geocode/1 returning error message" do
+    error = get_geocode("?")
+    assert error =~ Jason.encode!(%{msg: "Invalid input"})
+  end
 end
