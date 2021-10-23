13fef39220e95a81d375497c5f845560828b2726
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-23 19:38:45 -0400

    implement new functionalities for get_geocode function

--- lib/get_geocode/builder.ex
+++ lib/get_geocode/builder.ex
@@ -1,26 +1,85 @@
 defmodule GetGeocode.Builder do
-    alias GetGeocode.Apis.{ViaCep, Nominatim}
-    
-    def get_geocode(input) do
-        cond do
-            is_cep(input) -> get_viacep(input)
-            is_addr(input) -> get_nominatim(input)
-            true -> %{msg: "Invalid input"}
-        end
-    end
+  alias GetGeocode.Apis.{ViaCep, Nominatim}
+  alias GetGeocode.Geocode
 
-    defp get_viacep(cep) do
-        ViaCep.get_cep(cep)
+  def get_geocode(input) do
+    cond do
+      is_cep?(input) -> get_viacep(input)
+      is_addr?(input) -> get_nominatim(input)
+      true -> Jason.encode!(%{msg: "Invalid input"})
     end
-    defp get_nominatim(addr) do
-        Nominatim.get_data(addr)
-    end
-    defp is_addr(addr) do
-        addr
-        |> String.match?(~r/([a-zA-Z]+[\s|,]+[a-zA-Z]+[\s]*)/)
-    end
-    defp is_cep(cep) do
-        cep
-        |> String.match?(~r/\b([0-9]{5}-?[0-9]{3})$/)
-    end
+  end
+
+  defp get_viacep(cep) do
+    ViaCep.get_cep(cep)
+    |> builder_from_viacep()
+  end
+
+  defp get_nominatim(addr) do
+    Nominatim.get_data(addr)
+    |> builder_from_nominatim()
+  end
+
+  defp is_addr?(addr) do
+    addr
+    |> String.match?(~r/([a-zA-Z]+[\s|,]+[a-zA-Z]+[\s]*)/)
+  end
+
+  defp is_cep?(cep) do
+    cep
+    |> String.match?(~r/\b([0-9]{5}-?[0-9]{3})$/)
+  end
+
+  defp builder_from_viacep(result) do
+    %{
+      "bairro" => neighborhood,
+      "cep" => postalcode,
+      "localidade" => city,
+      "logradouro" => street,
+      "uf" => state
+    } = result
+
+    %{
+      "lon" => lng,
+      "lat" => lat,
+      "display_name" => full_details
+    } =
+      Regex.replace(~r/(.)\1+/, ~s[#{street},#{neighborhood},#{city}], "\\1")
+      |> Nominatim.get_data()
+
+    Jason.encode!(%Geocode{
+      postalcode: postalcode,
+      street: street,
+      state: state,
+      city: city,
+      neighborhood: neighborhood,
+      lng: lng,
+      lat: lat,
+      full_details: full_details
+    })
+  end
+
+  defp builder_from_nominatim(result) do
+    %{
+      "display_name" => full_details,
+      "lat" => lat,
+      "lon" => lng
+    } = result
+
+    [street, neighborhood, city, _, _, state, _, postalcode | _] =
+      full_details
+      |> String.split(",")
+      |> Enum.map(fn x -> String.trim(x) end)
+
+    Jason.encode!(%Geocode{
+      postalcode: postalcode,
+      street: street,
+      state: state,
+      city: city,
+      neighborhood: neighborhood,
+      lng: lng,
+      lat: lat,
+      full_details: full_details
+    })
+  end
 end
