b665067d358d2dfc2273cbd4ba45f23ef7f6838f
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-24 21:08:48 -0400

    constructor function to create a Geocode struct more clearly

--- lib/get_geocode/geocode.ex
+++ lib/get_geocode/geocode.ex
@@ -10,4 +10,12 @@ defmodule GetGeocode.Geocode do
     :lng,
     :full_details
   ]
+
+  def constructor(inputs) do
+    values_tuple =
+      List.delete_at(Map.keys(%GetGeocode.Geocode{}), 0)
+      |> Enum.zip(inputs)
+
+    struct(GetGeocode.Geocode, values_tuple)
+  end
 end
