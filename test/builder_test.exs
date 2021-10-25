d481b602a466e1150ea27e571dcc778e2b1b54f0
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-24 21:09:42 -0400

    implement test for new changes in builder and geocode files

--- test/builder_test.exs
+++ test/builder_test.exs
@@ -2,21 +2,37 @@ defmodule GetGeocodeBuilderTest do
   use ExUnit.Case, async: true
   import GetGeocode.Builder
 
-  @input_viacep "69035350"
-  @input_nominatim "Rua Izaurina Braga, Compensa, Amazonas"
+  setup do
+    %{cep: "69035350"}
+  end
 
-  test "get_geocode/1 from viacep" do
-    result = get_geocode(@input_viacep)
+  test "get_geocode/2 from viacep", %{cep: cep} do
+    result = get_geocode(cep)
     assert result =~ "postalcode"
   end
 
-  test "get_geocode/1 from nominatim" do
-    result = get_geocode(@input_nominatim)
+  test "get_geocode/2 from nominatim" do
+    result = get_geocode("Rua Izaurina Braga")
     assert result =~ "lng"
   end
 
-  test "get_geocode/1 returning error message" do
+  test "get_geocode/2 returning error message" do
     error = get_geocode("?")
     assert error =~ Jason.encode!(%{msg: "Invalid input"})
+  end
+
+  test "get_geocode/2 returning error message in map format" do
+    error = get_geocode("?", :map)
+    assert is_map(error)
+  end
+
+  test "get_geocode/2 with map format", %{cep: cep} do
+    values_map = get_geocode(cep, :map)
+    assert is_map(values_map)
+  end
+
+  test "get_geocode/2 with invalid format", %{cep: cep} do
+    values_map = get_geocode(cep, :foo)
+    assert values_map =~ "Invalid format!"
   end
 end
