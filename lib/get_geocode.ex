237e298daff5684cdef35af692a7af34392843b1
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-26 22:54:59 -0400

    new changes for module struct more clear

--- lib/get_geocode.ex
+++ lib/get_geocode.ex
@@ -1,14 +1,99 @@
 defmodule GetGeocode do
-  @moduledoc """
-  Documentation for `GetGeocode`.
-  """
+  alias GetGeocode.Apis.{ViaCep, Nominatim}
+  alias GetGeocode.Geocode
 
-  @doc """
-  Hello world.
+  def get(input) when is_binary(input) do
+    cond do
+      cep?(input) -> get_viacep(input)
+      addr?(input) -> get_nominatim(input)
+      true -> msg_invalid_input()
+    end
+  end
 
-  ## Examples
+  def get(_) do
+    msg_invalid_input()
+  end
 
-      iex> GetGeocode.hello()
-      :world
-  """
+  def get() do
+    msg_invalid_input()
+  end
+
+  defp get_viacep(cep) do
+    ViaCep.get_cep(cep)
+    |> builder_from_viacep()
+  end
+
+  defp get_nominatim(addr) do
+    Nominatim.get_data(addr)
+    |> builder_from_nominatim()
+  end
+
+  defp addr?(addr) do
+    addr
+    |> String.match?(~r/([a-zA-Z]+[\s|,]+[a-zA-Z]+[\s]*)/)
+  end
+
+  defp cep?(cep) do
+    cep
+    |> String.match?(~r/\b([0-9]{5}-?[0-9]{3})$/)
+  end
+
+  defp builder_from_viacep(result) do
+    %{
+      "bairro" => neighborhood,
+      "cep" => postalcode,
+      "localidade" => city,
+      "logradouro" => street,
+      "uf" => state
+    } = result
+
+    %{
+      "lon" => lng,
+      "lat" => lat,
+      "display_name" => full_details
+    } =
+      Regex.replace(~r/(.)\1+/, ~s[#{street},#{neighborhood},#{city}], "\\1")
+      |> Nominatim.get_data()
+
+    {:ok,
+     %Geocode{
+       postalcode: postalcode,
+       street: street,
+       neighborhood: neighborhood,
+       city: city,
+       state: state,
+       lat: lat,
+       lng: lng,
+       full_details: full_details
+     }}
+  end
+
+  defp builder_from_nominatim(result) do
+    %{
+      "display_name" => full_details,
+      "lat" => lat,
+      "lon" => lng
+    } = result
+
+    [street, neighborhood, city, _, _, state, _, postalcode | _] =
+      full_details
+      |> String.split(",")
+      |> Enum.map(fn x -> String.trim(x) end)
+
+    {:ok,
+     %Geocode{
+       postalcode: postalcode,
+       street: street,
+       neighborhood: neighborhood,
+       city: city,
+       state: state,
+       lat: lat,
+       lng: lng,
+       full_details: full_details
+     }}
+  end
+
+  defp msg_invalid_input() do
+    {:error, "Invalid input"}
+  end
 end
