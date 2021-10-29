c419099f077147c947a4eae8f1d56679a1953a27
ericklima-ca <ericklima.ca@yahoo.com>
2021-10-28 21:54:14 -0400

    finish documentation

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule GetGeocode.MixProject do
   def project do
     [
       app: :get_geocode,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
 
       # Doc
@@ -39,15 +41,13 @@ defmodule GetGeocode.MixProject do
     "A simple API to get geodata from CEP (brazilian) or full address format."
   end
 
-  # defp package() do
-  #   [
-  #     # This option is only needed when you don't want to use the OTP application name
-  #     name: "postgrex",
-  #     # These are the default files included in the package
-  #     files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-  #               license* CHANGELOG* changelog* src),
-  #     licenses: ["Apache-2.0"],
-  #     links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
-  #   ]
-  # end
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "get_geocode",
+      maintainer: ["Erick Amorim"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ericklima-ca/get_geocode"}
+    ]
+  end
 end
