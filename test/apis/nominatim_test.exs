be22b6b01677c92146f501d3af0247559eb736eb
ericklima-ca <ericklima.ca@yahoo.com>
2021-11-03 23:56:14 -0400

    tests for geocode reverse

--- test/apis/nominatim_test.exs
+++ test/apis/nominatim_test.exs
@@ -4,12 +4,19 @@ defmodule GetGeocodeApisNominatimTest do
 
   setup do
     %{
-      addr: "Rua Izaurina Braga, Compensa, Manaus"
+      addr: "Rua Izaurina Braga, Compensa, Manaus",
+      coords: {-3.0999329, -60.0552931}
     }
   end
 
   test "test get_data/1 result against lat, lon from response", %{addr: addr} do
     result = Nominatim.get_data(addr)
+    assert Map.has_key?(result, "lon")
+  end
+
+  test "test get_data/1 against input being tuple with lat, lng", %{coords: coords} do
+    result = Nominatim.get_data(coords)
+    assert Map.has_key?(result, "lat")
     assert Map.has_key?(result, "lon")
   end
 end
